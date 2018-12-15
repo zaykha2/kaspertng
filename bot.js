de0ec38947e915e774ff314487581a1ea2cc17ec
zaykha2 <45631038+zaykha2@users.noreply.github.com>
2018-12-15 02:12:29 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`جاكس المفشلق ♥ ,`,"http://twitch.tv/Death Shop")
+client.user.setGame(`#666 Record ♥ ,`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
